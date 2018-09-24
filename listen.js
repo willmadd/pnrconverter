ad763da9891debe746d8f9ec72b3cbfd7a17be67
William Maddicott <williammaddicott@Williams-MacBook-Pro.local>
2018-09-24 19:24:10 +0100

    port reverted back to 3000

--- listen.js
+++ listen.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 3000;
 
 app.listen(port,()=>{
     console.log(`listening on port ${port}...`)
